e65bfe422653f2c01c8c50f9ba45ada441c1f070
Your Name <yasarm8675@gmail.com>
2026-08-16 16:44:09 +0530

    updatem app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 export default function App() {
   const { scrollYProgress } = useScroll();
